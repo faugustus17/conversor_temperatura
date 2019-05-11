602499ad63d82b5af531e890fac61420f9b98093
Fernando Guimarães <faugustus17@gmail.com>
2019-05-11 14:09:17 -0300

    Validando entrada

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController celsiusController = TextEditingController();
+  TextEditingController fahrenheitController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields(){
+    celsiusController.text = "";
+    fahrenheitController.text = "";
+  }
+
+  void _converter(){
+    double celsius = double.parse(celsiusController.text);
+    double fahrenheit = celsius * 1.8 + 32.0;
+    fahrenheitController.text = fahrenheit.toStringAsFixed(4);
+  }
+
   @override
   Widget build(BuildContext context) {
     AppBar appBar = AppBar(
@@ -19,15 +35,35 @@ class _HomeState extends State<Home> {
       actions: <Widget>[
         IconButton(
           icon: Icon(Icons.refresh),
-          onPressed: () {},)
+          onPressed: _resetFields,)
       ],
     );
 
     Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
     TextStyle styleDecoration = TextStyle(color: Colors.blueAccent, fontSize: 20);
-    TextStyle styleField = TextStyle(color: Colors.blueAccent);
+    TextStyle styleField = TextStyle(color: Colors.red);
 
-    TextField tempCelsius = TextField(
+    RaisedButton raisedButton = RaisedButton(
+      onPressed: () {
+        if (_formKey.currentState.validate()){
+          _converter();
+        }
+      },
+      child: Text("Calcular"),
+      color: Colors.blueAccent
+    );
+
+    Container containerBtn = Container(
+      height: 200.0,
+      child: raisedButton,
+    );
+
+    Padding padding = Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: containerBtn,
+    );
+
+    TextFormField tempCelsius = TextFormField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
           labelText: "Temperatura em graus Celsius",
@@ -35,8 +71,14 @@ class _HomeState extends State<Home> {
       ),
       textAlign: TextAlign.center,
       style: styleField,
+      controller: celsiusController,
+      validator: (value){
+        if(value.isEmpty){
+          return "Informe um valor";
+        }
+      },
     );
-    TextField tempFahrenheit = TextField(
+    TextFormField tempFahrenheit = TextFormField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
           labelText: "Temperatura em graus Fahrenheit",
@@ -44,19 +86,35 @@ class _HomeState extends State<Home> {
       ),
       textAlign: TextAlign.center,
       style: styleField,
+      controller: fahrenheitController,
+      validator: (value){
+        if(value.isEmpty){
+          return "Informe um valor";
+        }
+      },
     );
 
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          icon, tempCelsius, tempFahrenheit,
+          icon, tempCelsius, tempFahrenheit, padding,
         ],
+    );
+
+    Form form = Form(
+      child: column,
+      key: _formKey,
+    );
+
+    SingleChildScrollView singleChildScrollView = SingleChildScrollView(
+      child: form,
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
     );
 
     Scaffold scaffold = Scaffold(
       appBar: appBar,
       backgroundColor: Colors.white,
-      body: column,
+      body: singleChildScrollView,
     );
 
     return scaffold;
